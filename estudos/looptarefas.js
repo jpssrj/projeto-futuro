42546526a6572bde6a15613f897e7f1d6c0e3e57
João Santos <jpss.rj14@gmail.com>
2026-04-02 11:25:33 -0300

    estd04

---
+++ estudos/looptarefas.js
@@ -0,0 +1,21 @@
+// 1. O Filtro Duplo (E / &&)
+// Percorra o array e dê um console.log apenas nas atividades que estão com status: "pendente" E têm prioridade: "alta".
+
+const tarefas = [
+    { id: 1, atividade: "Estudar JavaScript", status: "concluído", prioridade: "alta" },
+    { id: 2, atividade: "Fazer exercícios de Logística", status: "pendente", prioridade: "media" },
+    { id: 3, atividade: "Configurar ambiente Node", status: "pendente", prioridade: "alta" },
+    { id: 4, atividade: "Assistir aula do UNASP", status: "concluído", prioridade: "baixa" },
+    { id: 5, atividade: "Ler documentação de React", status: "pendente", prioridade: "media" },
+    { id: 6, atividade: "Atualizar o Dashboard de internet", status: "em progresso", prioridade: "alta" },
+    { id: 7, atividade: "Revisar Arquitetura de Computadores", status: "pendente", prioridade: "alta" },
+    { id: 8, atividade: "Ajustar microfone do projeto Scala", status: "concluído", prioridade: "baixa" },
+    { id: 9, atividade: "Backup do banco de dados", status: "pendente", prioridade: "media" },
+    { id: 10, atividade: "Reunião de equipe", status: "em progresso", prioridade: "baixa" }
+];
+
+for (let i = 0; i < tarefas.length; i++){
+    if (tarefas[i].status == 'pendente' && tarefas[i].prioridade == 'alta'){
+        console.log(`Você deve focar em: ${tarefas[i].atividade}`);
+    }
+}
