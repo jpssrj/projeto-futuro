9b3461d71cd4e4871ce2bfec6744c822639695f1
João Santos <jpss.rj14@gmail.com>
2026-04-08 22:05:34 -0300

    teste tiposStrings

---
+++ estudos/udemy/TestesTipos.js
@@ -0,0 +1,11 @@
+
+//                 0123456789
+const umaString = "A triade eh um grupo de irmaoS";
+
+console.log(umaString.charAt('4'))
+console.log(umaString.concat(' do peito'))
+console.log(umaString.indexOf('eh'))
+console.log(umaString.lastIndexOf('irmaos'))
+console.log(umaString.match(/[A-Z]/g))
+console.log(umaString.search(/a/g))
+console.log(umaString.replace('A triade', 'Os meus irmaos'))
