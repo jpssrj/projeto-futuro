7ae1004c37041bbabc00c4a39ca54e66ef7e5e24
João Santos <jpss.rj14@gmail.com>
2026-04-10 19:40:05 -0300

    create MiniBanco

---
+++ estudos/MiniBanco/app.js
@@ -0,0 +1,39 @@
+import { usuarios } from "./usuario.js";
+import scanner from 'readline-sync';
+
+console.log(`= Bem Vindo ao Wadawel Unibanco =`);
+
+let inserirId = scanner.question(`Insira o id da sua conta: `);
+let inserirSenha =  scanner.question(`Insira a sua senha: `);
+const usuarioEncontrado = usuarios.find( u => u.id == inserirId);
+
+    if (usuarioEncontrado && usuarioEncontrado.senha == inserirSenha) {
+        console.log(`Olá, ${usuarioEncontrado.nome}.`);
+        console.log(`O que deseja fazer hoje?`);
+        console.log(`=============================`);
+        console.log(`1. Verificar meu saldo`);
+        console.log(`2. Realizar uma transação`);
+        console.log(`3. Transação via Cheque Especial`);
+        console.log(`=============================`);
+
+        const operacao = scanner.questionInt(`Digite a operacao que deseja: `);
+            switch (operacao){
+
+                case 1:
+                    console.log(`${usuarioEncontrado.nome}, seu saldo é de R$${usuarioEncontrado.saldo.toFixed(2)}.`);
+                    break;
+
+                case 2:
+                    scanner.question(`Para quem deseja realizar a transação? `);
+                    const valorOperacao = scanner.questionFloat(`Insira o valor: `);
+                    console.log(`Enviado! Seu saldo agora é de R$${usuarioEncontrado.saldo - Number(valorOperacao).toFixed(2)}.`);
+                    break;
+
+            }
+    } else {
+        console.log(`Tente Novamente`);
+    }
+
+
+
+
---
+++ estudos/MiniBanco/usuario.js
@@ -0,0 +1,5 @@
+export const usuarios = [
+    {id: 1, nome: "João", senha: 2020, saldo: 100},
+    {id: 2, nome: "Marcos", senha: 1234, saldo: 200},
+    {id: 3, nome: "Luis", senha: 5678, saldo: 300}
+]
