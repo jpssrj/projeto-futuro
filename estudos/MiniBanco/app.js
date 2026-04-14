7d6969af35dab548f2fb1b1868725d62ac65dbaa
João Santos <jpss.rj14@gmail.com>
2026-04-13 22:29:17 -0300

    UPD MiniBanco

--- estudos/MiniBanco/app.js
+++ estudos/MiniBanco/app.js
@@ -18,18 +18,26 @@ const usuarioEncontrado = usuarios.find( u => u.id == inserirId);
 
         const operacao = scanner.questionInt(`Digite a operacao que deseja: `);
             switch (operacao){
-
                 case 1:
                     console.log(`${usuarioEncontrado.nome}, seu saldo é de R$${usuarioEncontrado.saldo.toFixed(2)}.`);
                     break;
-
                 case 2:
                     scanner.question(`Para quem deseja realizar a transação? `);
                     const valorOperacao = scanner.questionFloat(`Insira o valor: `);
-                    console.log(`Enviado! Seu saldo agora é de R$${usuarioEncontrado.saldo - Number(valorOperacao).toFixed(2)}.`);
+                    if (valorOperacao > usuarioEncontrado.saldo) {
+                        console.log(`Você não tem saldo suficiente para realizar essa operação.`);
+                    } else {
+                        console.log(`Enviado! Seu saldo agora é de R$${usuarioEncontrado.saldo - Number(valorOperacao).toFixed(2)}.`);
+                    }
                     break;
                 case 3:
-
+                    const chequeQuestion = scanner.questionFloat(`Qual o valor da transferência que deseja realizar? `);
+                    const faltanteCheque = chequeQuestion - Number(usuarioEncontrado.saldo);
+                    if (chequeQuestion > usuarioEncontrado.saldo) {
+                        console.log(`Seu saldo é de ${usuarioEncontrado.saldo}, o cheque especial irá somar ${faltanteCheque} para completar a operção.`);
+                    } else {
+                        console.log(`Você não tem a necessidade de usar um cheque especial. Possui saldo suficiente para realizar essa operação.`);
+                    }
             }
     } else {
         console.log(`Tente Novamente`);
--- estudos/MiniBanco/usuario.js
+++ estudos/MiniBanco/usuario.js
@@ -1,5 +1,6 @@
 export const usuarios = [
     {id: 1, nome: "João", senha: 2020, saldo: 100},
     {id: 2, nome: "Marcos", senha: 1234, saldo: 200},
-    {id: 3, nome: "Luis", senha: 5678, saldo: 300}
+    {id: 3, nome: "Luis", senha: 5678, saldo: 300},
+    {id: 0, nome: "Julio", senha: 9090, saldo: 1000000000}
 ]
