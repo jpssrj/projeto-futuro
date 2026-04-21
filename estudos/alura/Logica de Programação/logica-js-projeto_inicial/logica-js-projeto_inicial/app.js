98f465cf5d2d82943f43a48929aa261e228c38a2
João Santos <jpss.rj14@gmail.com>
2026-04-21 13:12:43 -0300

    qnt tentativas

--- estudos/alura/Logica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ estudos/alura/Logica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -4,6 +4,7 @@ let numeroSecreto = 4;
 console.log(`O número secreto é: ${numeroSecreto}`);
 
 let chute;
+let tentativas = 1;
 console.log(`O número chutado foi: ${chute}`);
 
 // Comparação para true or false
@@ -15,13 +16,15 @@ while (chute != numeroSecreto) {
     // Se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
         console.log("O número está certo, você acertou o chute!");
-        alert("Você acertou!");
+        alert(`Você acertou o número secreto com ${tentativas} tentativas`);
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
         } else {
             alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas++;
+        console.log(tentativas);
     }
 }
 
