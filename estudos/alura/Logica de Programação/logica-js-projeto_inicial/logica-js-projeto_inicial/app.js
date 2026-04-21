62a05bac749389a06f4dd69ec8807526cdcc47c4
João Santos <jpss.rj14@gmail.com>
2026-04-21 13:06:01 -0300

    feat while

--- estudos/alura/Logica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ estudos/alura/Logica de Programação/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -3,17 +3,25 @@ alert("Boas vindas ao jogo do número secreto!");
 let numeroSecreto = 4;
 console.log(`O número secreto é: ${numeroSecreto}`);
 
-let chute = Number(prompt(`Insira o número do seu chute: `));
+let chute;
 console.log(`O número chutado foi: ${chute}`);
 
 // Comparação para true or false
 console.log('Resultado da comparação:', chute == numeroSecreto);
 
-// Se o chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    console.log("O número está certo, você acertou o chute!");
-    alert("Você acertou!");
-} else {
-    console.log("Você errou :(");
-    alert("Você errou o chute :(");
+while (chute != numeroSecreto) {
+    chute = Number(prompt(`Insira o número do seu chute: `));
+
+    // Se o chute for igual ao numero secreto
+    if (chute == numeroSecreto) {
+        console.log("O número está certo, você acertou o chute!");
+        alert("Você acertou!");
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+    }
 }
+
