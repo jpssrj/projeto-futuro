4658d33c6cc6e60deef5813437785fbae1738e11
João Santos <jpss.rj14@gmail.com>
2026-04-21 15:23:00 -0300

    contadores

---
+++ estudos/alura/MiniDesafios/Contador++/app.js
@@ -0,0 +1,11 @@
+alert("Contador de 0 até 10");
+
+let contador = 0;
+
+while (contador <= 9) {
+    alert(`Contador ${contador}`);
+    contador++
+}
+
+alert(`O contador chegou até o valor de ${contador}`)
+
---
+++ estudos/alura/MiniDesafios/Contador--/app.js
@@ -0,0 +1,11 @@
+alert("Contador de 0 até 10");
+
+let contador = 10;
+
+while (contador > 0) {
+    alert(`Contador ${contador}`);
+    contador--
+}
+
+alert(`O contador chegou até o valor de ${contador}`)
+
