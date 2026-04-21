db4bbb09a6fc559eb5c8a32934b7122979dc03bd
João Santos <jpss.rj14@gmail.com>
2026-04-21 12:29:46 -0300

    pontuacao

---
+++ estudos/alura/MiniDesafios/Pontuacao/app.js
@@ -0,0 +1,9 @@
+alert("Bem vindo ao sistema de pontuação");
+
+let pontos = Number(prompt("Quantos pontos você fez? "));
+
+if (pontos >= 100) {
+    alert("Parabéns, você venceu!");
+} else {
+    alert("Tente novamente para ganhar");
+}
