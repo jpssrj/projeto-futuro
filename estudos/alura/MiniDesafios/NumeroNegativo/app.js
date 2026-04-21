ada1acc26b11f957b56fe30e9021174053622e89
João Santos <jpss.rj14@gmail.com>
2026-04-21 12:26:28 -0300

    num Negativo

--- estudos/alura/MiniDesafios/NumeroNegativo/app.js
+++ estudos/alura/MiniDesafios/NumeroNegativo/app.js
@@ -1,19 +1,9 @@
-alert("Boas vindas ao jogo do número secreto!");
+alert("Teste do número negativo!");
 
-let numeroSecreto = 4;
-console.log(`O número secreto é: ${numeroSecreto}`);
+let num = Number(prompt("Insira um número: "));
 
-let chute = Number(prompt(`Insira o número do seu chute: `));
-console.log(`O número chutado foi: ${chute}`);
-
-// Comparação para true or false
-console.log('Resultado da comparação:', chute == numeroSecreto);
-
-// Se o chute for igual ao numero secreto
-if (chute == numeroSecreto) {
-    console.log("O número está certo, você acertou o chute!");
-    alert("Você acertou!");
+if (num < 0) {
+    alert(num + " É um número negativo");
 } else {
-    console.log("Você errou :(");
-    alert("Você errou o chute :(");
+    alert(num + " Não é um número negativo");
 }
