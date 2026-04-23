5d8a4684b85c81910175349d63f7520aa99cb646
João Santos <jpss.rj14@gmail.com>
2026-04-23 16:14:42 -0300

    adição desafio prático

---
+++ estudos/alura/Logica de Programação/explore-funcoes-e-listas/app.js
@@ -0,0 +1,5 @@
+let titulo = document.getElementById("tittle");
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let subTitulo = document.querySelector('.texto__paragrafo');
+subTitulo.innerHTML = 'Escolha um número entre 1 e 10';
