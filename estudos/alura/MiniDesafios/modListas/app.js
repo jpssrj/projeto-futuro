00fb59c9d6077894228a3785458111b693e181b5
João Santos <jpss.rj14@gmail.com>
2026-04-28 08:00:10 -0300

    Create app.js

---
+++ estudos/alura/MiniDesafios/modListas/app.js
@@ -0,0 +1,13 @@
+let listaGenerica = [];
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby', 'GoLang');
+let doisItens = linguagensDeProgramacao.slice(4)
+
+console.log(linguagensDeProgramacao)
+console.log(linguagensDeProgramacao.length)
+console.log(linguagensDeProgramacao[1]);
+console.log(doisItens);
+console.log(linguagensDeProgramacao.at(-1));
